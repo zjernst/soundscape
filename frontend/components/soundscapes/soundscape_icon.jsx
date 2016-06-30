7341a08f6440bf3f7f7be98b4fa861361e0e7d8e
zachary_ernst <zjernst@gmail.com>
2016-06-29 21:58:01 -0700

    working index

--- frontend/components/soundscapes/soundscape_icon.jsx
+++ frontend/components/soundscapes/soundscape_icon.jsx
@@ -3,6 +3,7 @@ const SoundscapeStore = require('../../stores/soundscape_store');
 const SoundscapeActions = require('../../actions/soundscape_actions');
 const SoundscapeIndexItem = require('./ss_index_item');
 const hashHistory = require('react-router').hashHistory;
+const classNames = require('classnames')
 
 const SoundscapeIcon = React.createClass({
   _redirect() {
@@ -11,9 +12,14 @@ const SoundscapeIcon = React.createClass({
   },
 
   render() {
+    let selected = ""
+    if (this.props.selected == this.props.soundscape.id) {
+      selected = "selected"
+    }
+    let iconClass = classNames('soundscape_icon', selected);
     return(
-      <div className="soundscape_icon" onClick={this._redirect}>
-        <img src={this.props.soundscape.image_url} height="100" width="100" />
+      <div className={iconClass} onClick={this._redirect}>
+        <img src={this.props.soundscape.image_url} height="80" width="80" />
       </div>
     )
   }
