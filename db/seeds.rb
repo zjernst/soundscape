3230820956dde2d5df06c896b677c1bf24a69899
Zachary Ernst <Zach@Zacharys-MacBook-Air.local>
2016-07-08 12:14:03 -0700

    db more seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,8 @@ Tag.create({name: 'round'})
 Tag.create({name: 'soft'})
 Tag.create({name: 'hard'})
 Tag.create({name: 'insects'})
+Tag.create({name: 'river'})
+Tag.create({name: 'voices'})
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
@@ -119,7 +121,16 @@ Tagging.create({track_id: 8, tag_id: 10})
 Tagging.create({track_id: 8, tag_id: 12})
 Tagging.create({track_id: 8, tag_id: 17})
 
-Track.create({title: 'around town', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467321148/Allegro_from_Duet_in_C_Major_jvdgmr.mp3', soundscape_id: 4, artist_id: 1})
+Track.create({title: 'Crouds', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467321148/Allegro_from_Duet_in_C_Major_jvdgmr.mp3',
+  description: "A sampling of a variety of crowds", soundscape_id: 4, artist_id: 1})
+
+Tagging.create({track_id: 9, tag_id: 5})
+Tagging.create({track_id: 9, tag_id: 6})
+Tagging.create({track_id: 9, tag_id: 11})
+Tagging.create({track_id: 9, tag_id: 10})
+Tagging.create({track_id: 9, tag_id: 24})
+Tagging.create({track_id: 9, tag_id: 21})
+
 Track.create({title: 'Forest Stream', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467350452/132049__kolezan__water-stream-5_kkilf2.mp3',
   description: "Recording of a meandering stream in a forest. Short track, but very peaceful!", soundscape_id: 5, artist_id: 5})
 
@@ -147,8 +158,23 @@ Tagging.create({track_id: 12, tag_id: 12})
 Tagging.create({track_id: 12, tag_id: 4})
 Tagging.create({track_id: 12, tag_id: 14})
 
-Track.create({title: 'home sweet home', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467321148/Allegro_from_Duet_in_C_Major_jvdgmr.mp3', soundscape_id: 6, artist_id: 1})
-Track.create({title: 'flickering flame', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467321148/Allegro_from_Duet_in_C_Major_jvdgmr.mp3', soundscape_id: 7, artist_id: 1})
+Track.create({title: 'Conversations of Home', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467988202/237536__joangilbardaji__a-conversation-in-a-musulman-family-mn_aoqoc0.wav',
+  description: 'just a normal conversation in my household', soundscape_id: 6, artist_id: 6})
+
+Tagging.create({track_id: 13, tag_id: 16})
+Tagging.create({track_id: 13, tag_id: 18})
+Tagging.create({track_id: 13, tag_id: 21})
+Tagging.create({track_id: 13, tag_id: 11})
+
+Track.create({title: 'Flickering Flame', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467321148/Allegro_from_Duet_in_C_Major_jvdgmr.mp3',
+  description: 'gentle sound of crackling fire', soundscape_id: 7, artist_id: 1})
+
+Tagging.create({track_id: 14, tag_id: 3})
+Tagging.create({track_id: 14, tag_id: 8})
+Tagging.create({track_id: 14, tag_id: 20})
+Tagging.create({track_id: 14, tag_id: 4})
+Tagging.create({track_id: 14, tag_id: 1})
+
 Track.create({title: 'Active Coffee', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467594693/coffee/216138__somersethypnotherapy__ambient-coffee-shop-sounds.mp3',
   description: 'Busy afternoon in a German coffee shop. Lots of bustle and activity.', soundscape_id: 8, artist_id: 3})
 
@@ -279,10 +305,218 @@ Tagging.create({track_id: 27, tag_id: 22})
 Track.create({title: 'Aquatic Sound of the Sea', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467584201/FelixBlumePack/328306__felix-blume__aquatic-sound-of-the-sea-from-the-boat-close-to-punta-allen-recife-in-the-sian-ka-an-biosphere-reserve.mp3',
   description: "Aquatic sound of the sea from the boat close to Punta Allen Recife in the Sian Ka'an Biosphere", soundscape_id: 2, artist_id: 4})
 
-Tagging.create({track_id: 28, tag_id: 4})
-Tagging.create({track_id: 28, tag_id: 1})
-Tagging.create({track_id: 28, tag_id: 2})
-Tagging.create({track_id: 28, tag_id: 12})
-Tagging.create({track_id: 28, tag_id: 13})
-Tagging.create({track_id: 28, tag_id: 8})
-Tagging.create({track_id: 28, tag_id: 20})
+  Tagging.create({track_id: 28, tag_id: 4})
+  Tagging.create({track_id: 28, tag_id: 1})
+  Tagging.create({track_id: 28, tag_id: 2})
+  Tagging.create({track_id: 28, tag_id: 12})
+  Tagging.create({track_id: 28, tag_id: 13})
+  Tagging.create({track_id: 28, tag_id: 8})
+  Tagging.create({track_id: 28, tag_id: 20})
+
+Track.create({title: 'River Running', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467989121/196699__vonfleisch__river-running-water-2_1_bd0i1l.wav',
+  description: "Lovely track of a river.", soundscape_id: 5, artist_id: 2})
+
+Tagging.create({track_id: 29, tag_id: 23})
+Tagging.create({track_id: 29, tag_id: 4})
+Tagging.create({track_id: 29, tag_id: 8})
+Tagging.create({track_id: 29, tag_id: 12})
+Tagging.create({track_id: 29, tag_id: 16})
+Tagging.create({track_id: 29, tag_id: 20})
+
+Track.create({title: 'Tidepool', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467989056/332171__pyro13djt__water-in-a-pond_ng0co9.ogg',
+  description: "Playing in a tidepool on the Pacific.", soundscape_id: 2, artist_id: 2})
+
+Tagging.create({track_id: 30, tag_id: 1})
+Tagging.create({track_id: 30, tag_id: 2})
+Tagging.create({track_id: 30, tag_id: 8})
+Tagging.create({track_id: 30, tag_id: 12})
+Tagging.create({track_id: 30, tag_id: 16})
+Tagging.create({track_id: 30, tag_id: 20})
+
+
+Track.create({title: 'Edge of the Forest', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467988064/135135__inchadney__at-the-edge-of-the-forest_qcyozy.wav',
+  description: "I'm standing on the Edge.", soundscape_id: 5, artist_id: 4})
+
+Tagging.create({track_id: 31, tag_id: 5})
+Tagging.create({track_id: 31, tag_id: 12})
+Tagging.create({track_id: 31, tag_id: 13})
+Tagging.create({track_id: 31, tag_id: 1})
+Tagging.create({track_id: 31, tag_id: 15})
+Tagging.create({track_id: 31, tag_id: 4})
+
+Track.create({title: 'Old Cassette Player', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467987479/191840__klankbeeld__old-womans-cassette-player-1987_pqdyw7.mp3',
+  description: "Conversing over dinner", soundscape_id: 6, artist_id: 4})
+
+Tagging.create({track_id: 32, tag_id: 19})
+Tagging.create({track_id: 32, tag_id: 18})
+Tagging.create({track_id: 32, tag_id: 11})
+Tagging.create({track_id: 32, tag_id: 7})
+
+Track.create({title: 'Stories of War', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467987479/191840__klankbeeld__old-womans-cassette-player-1987_pqdyw7.mp3',
+  description: "Barcelona resident recalling stories of war", soundscape_id: 6, artist_id: 4})
+
+Tagging.create({track_id: 33, tag_id: 6})
+Tagging.create({track_id: 33, tag_id: 7})
+Tagging.create({track_id: 33, tag_id: 11})
+Tagging.create({track_id: 33, tag_id: 21})
+Tagging.create({track_id: 33, tag_id: 18})
+
+Track.create({title: 'Dance Practice', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467987479/191840__klankbeeld__old-womans-cassette-player-1987_pqdyw7.mp3',
+  description: "Recording of a local dance class", soundscape_id: 6, artist_id: 6})
+
+Tagging.create({track_id: 34, tag_id: 6})
+Tagging.create({track_id: 34, tag_id: 7})
+Tagging.create({track_id: 34, tag_id: 11})
+Tagging.create({track_id: 34, tag_id: 24})
+Tagging.create({track_id: 34, tag_id: 18})
+
+Track.create({title: 'Setting up Christmas Eve', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467987035/111141__miastodzwiekow__preparing-christmaseve-table-241210_okudxl.mp3',
+  description: "Getting ready for a family dinner", soundscape_id: 6, artist_id: 6})
+
+Tagging.create({track_id: 35, tag_id: 6})
+Tagging.create({track_id: 35, tag_id: 7})
+Tagging.create({track_id: 35, tag_id: 11})
+Tagging.create({track_id: 35, tag_id: 24})
+Tagging.create({track_id: 35, tag_id: 19})
+
+Track.create({title: 'Carnival', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467987035/111141__miastodzwiekow__preparing-christmaseve-table-241210_okudxl.mp3',
+  description: "Carnival ambiance recorded in Holland", soundscape_id: 4, artist_id: 1})
+
+Tagging.create({track_id: 36, tag_id: 6})
+Tagging.create({track_id: 36, tag_id: 3})
+Tagging.create({track_id: 36, tag_id: 11})
+Tagging.create({track_id: 36, tag_id: 24})
+Tagging.create({track_id: 36, tag_id: 1})
+
+Track.create({title: 'Walk Thru Shop', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467987035/111141__miastodzwiekow__preparing-christmaseve-table-241210_okudxl.mp3',
+  description: "Walking down the streets and listening to vendors", soundscape_id: 4, artist_id: 3})
+
+Tagging.create({track_id: 37, tag_id: 6})
+Tagging.create({track_id: 37, tag_id: 17})
+Tagging.create({track_id: 37, tag_id: 11})
+Tagging.create({track_id: 37, tag_id: 24})
+Tagging.create({track_id: 37, tag_id: 16})
+
+Track.create({title: 'Traffic', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467963467/city/173154__inchadney__traffic.mp3',
+  description: "Traffic", soundscape_id: 4, artist_id: 5})
+
+Tagging.create({track_id: 38, tag_id: 7})
+Tagging.create({track_id: 38, tag_id: 17})
+Tagging.create({track_id: 38, tag_id: 11})
+Tagging.create({track_id: 38, tag_id: 21})
+Tagging.create({track_id: 38, tag_id: 16})
+
+Track.create({title: 'More traffic', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467963465/city/110310__nickpursehouse__traffic-medium-city-dry-road-xy-90-degrees-recording.mp3',
+  description: "More traffic", soundscape_id: 4, artist_id: 5})
+
+Tagging.create({track_id: 39, tag_id: 7})
+Tagging.create({track_id: 39, tag_id: 17})
+Tagging.create({track_id: 39, tag_id: 11})
+Tagging.create({track_id: 39, tag_id: 21})
+Tagging.create({track_id: 39, tag_id: 16})
+
+Track.create({title: 'Under the Underpass', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467963434/city/160001__klangfabrik__highwayunderpass.mp3',
+  description: "More traffic", soundscape_id: 4, artist_id: 1})
+
+Tagging.create({track_id: 40, tag_id: 8})
+Tagging.create({track_id: 40, tag_id: 17})
+Tagging.create({track_id: 40, tag_id: 3})
+Tagging.create({track_id: 40, tag_id: 19})
+Tagging.create({track_id: 40, tag_id: 16})
+
+Track.create({title: 'Morocco Marketplace', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467987035/111141__miastodzwiekow__preparing-christmaseve-table-241210_okudxl.mp3',
+  description: "Walking down the streets and listening to vendors", soundscape_id: 4, artist_id: 1})
+
+Tagging.create({track_id: 41, tag_id: 6})
+Tagging.create({track_id: 41, tag_id: 17})
+Tagging.create({track_id: 41, tag_id: 11})
+Tagging.create({track_id: 41, tag_id: 24})
+Tagging.create({track_id: 41, tag_id: 3})
+Tagging.create({track_id: 41, tag_id: 4})
+Tagging.create({track_id: 41, tag_id: 8})
+
+Track.create({title: 'Sazman Marketplace', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467963339/city/7890__sazman__marketplace.mp3',
+  description: "Food markey in Sazman. Lots going on! Busy and loud", soundscape_id: 4, artist_id: 5})
+
+Tagging.create({track_id: 42, tag_id: 21})
+Tagging.create({track_id: 42, tag_id: 16})
+Tagging.create({track_id: 42, tag_id: 11})
+Tagging.create({track_id: 42, tag_id: 24})
+Tagging.create({track_id: 42, tag_id: 3})
+Tagging.create({track_id: 42, tag_id: 5})
+Tagging.create({track_id: 42, tag_id: 10})
+
+Track.create({title: 'Chongnyangyi Market', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467963339/city/7890__sazman__marketplace.mp3',
+  description: "Another market", soundscape_id: 4, artist_id: 5})
+
+Tagging.create({track_id: 43, tag_id: 21})
+Tagging.create({track_id: 43, tag_id: 10})
+Tagging.create({track_id: 43, tag_id: 11})
+Tagging.create({track_id: 43, tag_id: 24})
+Tagging.create({track_id: 43, tag_id: 3})
+Tagging.create({track_id: 43, tag_id: 5})
+
+Track.create({title: 'Saturday Night Revelry', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467963325/city/106905__bulldozia__saturday-night-revelry.mp3',
+  description: "Loud and active Saturday night in the city", soundscape_id: 4, artist_id: 6})
+
+Tagging.create({track_id: 44, tag_id: 21})
+Tagging.create({track_id: 44, tag_id: 10})
+Tagging.create({track_id: 44, tag_id: 11})
+Tagging.create({track_id: 44, tag_id: 24})
+Tagging.create({track_id: 44, tag_id: 3})
+Tagging.create({track_id: 44, tag_id: 5})
+Tagging.create({track_id: 44, tag_id: 6})
+
+Track.create({title: 'Picnic Fire', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467614579/fire/54945__satoration__preparing-fire-at-the-picnic.mp3',
+  description: 'Preparing a fire for a weekend picnic with family', soundscape_id: 7, artist_id: 1})
+
+Tagging.create({track_id: 45, tag_id: 3})
+Tagging.create({track_id: 45, tag_id: 7})
+Tagging.create({track_id: 45, tag_id: 20})
+Tagging.create({track_id: 45, tag_id: 4})
+Tagging.create({track_id: 45, tag_id: 1})
+
+Track.create({title: 'Winter Heat', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467614576/fire/46272__phreaksaccount__fire-medium-loop.mp3',
+  description: 'Building a fire to keep warm in the harsh winter', soundscape_id: 7, artist_id: 3})
+
+Tagging.create({track_id: 46, tag_id: 3})
+Tagging.create({track_id: 46, tag_id: 10})
+Tagging.create({track_id: 46, tag_id: 20})
+Tagging.create({track_id: 46, tag_id: 4})
+Tagging.create({track_id: 46, tag_id: 1})
+
+Track.create({title: 'Crackling Fireplace', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467614570/fire/60568__robinhood76__00370-fireplace-1.mp3',
+  description: 'Fireplace crackles and pops! Great ambiance, might be a bit distracting to some though.', soundscape_id: 7, artist_id: 1})
+
+Tagging.create({track_id: 47, tag_id: 7})
+Tagging.create({track_id: 47, tag_id: 10})
+Tagging.create({track_id: 47, tag_id: 20})
+Tagging.create({track_id: 47, tag_id: 5})
+Tagging.create({track_id: 47, tag_id: 1})
+
+Track.create({title: 'Campfire', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467614576/fire/46272__phreaksaccount__fire-medium-loop.mp3',
+  description: 'Short sample of a fire while camping. Lovely quality, just with my phone had more battery to record more!', soundscape_id: 7, artist_id: 3})
+
+Tagging.create({track_id: 48, tag_id: 3})
+Tagging.create({track_id: 48, tag_id: 10})
+Tagging.create({track_id: 48, tag_id: 5})
+Tagging.create({track_id: 48, tag_id: 6})
+
+Track.create({title: 'Roaring Forest Fire', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467614555/fire/17548__dynamicell__fire-forest-inferno.mp3',
+  description: 'Short clip, but check out this awesome fire!', soundscape_id: 7, artist_id: 1})
+
+Tagging.create({track_id: 49, tag_id: 7})
+Tagging.create({track_id: 49, tag_id: 10})
+Tagging.create({track_id: 49, tag_id: 17})
+Tagging.create({track_id: 49, tag_id: 5})
+Tagging.create({track_id: 49, tag_id: 1})
+
+Track.create({title: 'Tidepool', track_url: 'https://res.cloudinary.com/soundscape/video/upload/v1467989056/332171__pyro13djt__water-in-a-pond_ng0co9.ogg',
+  description: "Playing in a tidepool on the Pacific.", soundscape_id: 2, artist_id: 2})
+
+Tagging.create({track_id: 50, tag_id: 1})
+Tagging.create({track_id: 50, tag_id: 2})
+Tagging.create({track_id: 50, tag_id: 8})
+Tagging.create({track_id: 50, tag_id: 12})
+Tagging.create({track_id: 50, tag_id: 16})
+Tagging.create({track_id: 50, tag_id: 20})
