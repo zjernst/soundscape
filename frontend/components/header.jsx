dd9c72986e4cf140460ed11446ac7b770ff227c1
zachary_ernst <zjernst@gmail.com>
2016-07-27 20:52:04 -0700

    parallax waaaave

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -1,14 +1,26 @@
 const React = require('react');
+import { Parallax } from 'react-parallax';
 
 const Header = React.createClass({
   render() {
-    return(
-      <section className="header_container">
-        <div className="header_background" />
-        <div className="header_background_text">Hear the world.</div>
-      </section>
+    return (
+      <div className="header_container">
+        <Parallax bgImage='https://res.cloudinary.com/soundscape/image/upload/v1467305571/foggy_forest_qmxcjc.jpg' strength={400}>
+          <div className="header_background_text">Hear the world.</div>
+        </Parallax>
+      </div>
     )
   }
 });
+
+// const Header = React.createClass({
+//   render() {
+//     return(
+//       <section className="header_container">
+//         <div className="header_background" />
+//       </section>
+//     )
+//   }
+// });
 
 module.exports = Header;
--- frontend/components/midsection.jsx
+++ frontend/components/midsection.jsx
@@ -1,14 +1,16 @@
 const React = require('react');
+import { Parallax } from 'react-parallax';
 
 const Midsection = React.createClass({
   render() {
     return(
       <div className="midsection_container">
-        <div className="midsection_background" />
+        <Parallax bgImage='https://res.cloudinary.com/soundscape/image/upload/v1467349002/beachwaves_qgvy7z.jpg' strength={300}>
         <div className="midsection_text">Find your sound.
          <p className="midsection_subtext">
           Filter by Soundscapes, Tags, and Artists to find the perfect sound.</p>
         </div>
+        </Parallax>
       </div>
     )
   }
